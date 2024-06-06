05d5f5c2e0cb2bfd08acfe73e9290f0f39ae8b2c
SonTungIT <son.tung.tn12321@gmail.com>
2024-06-06 21:38:57 +0700

    refactor: schema error

--- src/schemas/schemaCategory.ts
+++ src/schemas/schemaCategory.ts
@@ -4,10 +4,15 @@ export const schemaCategory = yup.object().shape({
     name: yup
         .string()
         .required('Tên thể loại không đuợc để trống')
-        .min(3, 'Tên thể loại phải có ít nhất 3 ký tự')
+        .min(1, 'Tên thể loại phải có ít nhất 3 ký tự')
         .max(50, 'Tên thể loại không được vượt quá 50 ký tự')
         .matches(
             /^[a-zA-Z0-9\s\u00C0-\u024F\u1E00-\u1EFF]*$/,
             'Tên thể loại không được chứa ký tự đặc biệt',
+        )
+        .test(
+            'should start with capital letter',
+            'Chữ cái đầu phải viết hoa',
+            (value) => !!value && value[0] === value[0].toUpperCase(),
         ),
 });
--- src/schemas/schemaIngredientType.ts
+++ src/schemas/schemaIngredientType.ts
@@ -4,7 +4,7 @@ export const schemaCreateIngredientTypes = yup.object().shape({
     name: yup
         .string()
         .required('Tên thể loại không đuợc để trống')
-        .min(3, 'Tên thể loại phải có ít nhất 3 ký tự')
+        .min(1, 'Tên thể loại phải có ít nhất 3 ký tự')
         .max(50, 'Tên thể loại không được vượt quá 50 ký tự')
         .matches(
             /^[a-zA-Z0-9\s\u00C0-\u024F\u1E00-\u1EFF]*$/,
