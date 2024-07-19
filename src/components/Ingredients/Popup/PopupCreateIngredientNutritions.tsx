e6ffa4d07ffa7c2380afadff39e087d4906ef0c3
NhutMinhIT <nhutminh.it19@gmail.com>
2024-07-19 22:18:50 +0700

    refactor: handle update nutrition in ingredient

--- src/components/Ingredients/Popup/PopupCreateIngredientNutritions.tsx
+++ src/components/Ingredients/Popup/PopupCreateIngredientNutritions.tsx
@@ -55,11 +55,11 @@ const PopupCreateIngredientNutritions = ({
         remove(index);
     };
 
-    const handleAddMore= () => {
-        if(fields.length <= 3) {
+    const handleAddMore = () => {
+        if (fields.length < 3) {
             append(0);
         }
-    }
+    };
 
     const onSubmit = (data: FormCreateIngredientNutritionsStepValues) => {
         setIsLoading(true);
@@ -88,7 +88,7 @@ const PopupCreateIngredientNutritions = ({
                         sx={{
                             fontWeight: "bold",
                         }}
-                    >Cập nhật dinh dưỡng cho nguyên liệu</DialogTitle>
+                    >Thêm dinh dưỡng cho nguyên liệu</DialogTitle>
                     <XMarkIcon
                         height={24}
                         width={24}
@@ -97,94 +97,93 @@ const PopupCreateIngredientNutritions = ({
                     />
                 </div>
                 <DialogContent>
-
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <input
-                        type="hidden"
-                        {...register("ingredientId")}
-                        value={ingredientId}
-                    />
-                    <div className="mb-4">
-                        <Grid container rowSpacing={2} columnSpacing={2}>
-                            <Grid item md={11} xs={11} xl={11} lg={11}>
-                                <label
-                                    htmlFor="id"
-                                    className="block w-full px-3 py-2 sm:text-sm"
-                                >
-                                    Loại dinh dưỡng
-                                </label>
-                            </Grid>
-                            <Grid item md={1} xs={1} xl={1} lg={1} />
-                                 </Grid>
-                                    <div className="overflow-y-auto h-80">
-                                        <Grid container>
-                                            {fields.map((field, index) => (
-                                                <Grid container key={field.id} rowSpacing={2} columnSpacing={2} sx={{ marginBottom: 2 }}>
-                                                    <Grid item md={11} xs={11} xl={11} lg={11} rowSpacing={2} columnSpacing={2}>
-                                                        <select
-                                                            {...register(`nutritionIds.${index}`)}
-                                                            className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-orange-500 focus:border-orange-500 sm:text-sm"
-                                                        >
-                                                            <option value="0">Chọn nguyên liệu</option>
-                                                            {nutritions && nutritions.map((nutrition) => (
-                                                                <option key={nutrition.id} value={nutrition.id}>
-                                                                    {nutrition.name}
-                                                                </option>
-                                                            ))}
-                                                        </select>
-                                                    </Grid>
-                                                    {fields.length === 1 ? '' : (
-                                                        <Grid item md={1} xs={1} xl={1} lg={1} alignSelf="center">
-                                                            <button
-                                                                type="button"
-                                                                onClick={() => handleRemove(index)}
-                                                                className="text-red-500 hover:text-red-700"
-                                                            >
-                                                                <XMarkIcon width={24} height={24} />
-                                                            </button>
-                                                        </Grid>
-                                                    )}
-                                                </Grid>
-                                            ))}
-                                        </Grid>
-                                    </div>
-                                </div>
-                                <button
-                                    type="button"
-                                    onClick={handleAddMore}
-                                    className="mt-4 bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 disabled:bg-gray-500"
-                                    disabled={fields?.length >= 3}
-                                >
-                                    Thêm dinh dưỡng
-                                </button>
-                                <div className="flex justify-end">
-                                    <button
-                                        type="submit"
-                                        className="bg-orange-500 text-white px-4 py-2 rounded-md hover:bg-orange-600"
-                                        disabled={isLoading}
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <input
+                            type="hidden"
+                            {...register("ingredientId")}
+                            value={ingredientId}
+                        />
+                        <div className="mb-4">
+                            <Grid container rowSpacing={2} columnSpacing={2}>
+                                <Grid item md={11} xs={11} xl={11} lg={11}>
+                                    <label
+                                        htmlFor="id"
+                                        className="block w-full px-3 py-2 sm:text-sm"
                                     >
-                                        {isLoading ? (
-                                            <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
-                                                <circle
-                                                    className="opacity-25"
-                                                    cx="12"
-                                                    cy="12"
-                                                    r="10"
-                                                    stroke="currentColor"
-                                                    strokeWidth="4"
-                                                ></circle>
-                                                <path
-                                                    className="opacity-75"
-                                                    fill="currentColor"
-                                                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                                                ></path>
-                                            </svg>
-                                        ) : (
-                                            "Xác nhận"
-                                        )}
-                                    </button>
-                                </div>
-                            </form>
+                                        Loại dinh dưỡng
+                                    </label>
+                                </Grid>
+                                <Grid item md={1} xs={1} xl={1} lg={1} />
+                            </Grid>
+                            <div className="overflow-y-auto h-80">
+                                <Grid container>
+                                    {fields.map((field, index) => (
+                                        <Grid container key={field.id} rowSpacing={2} columnSpacing={2} sx={{ marginBottom: 2 }}>
+                                            <Grid item md={11} xs={11} xl={11} lg={11} rowSpacing={2} columnSpacing={2}>
+                                                <select
+                                                    {...register(`nutritionIds.${index}`)}
+                                                    className="block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-orange-500 focus:border-orange-500 sm:text-sm"
+                                                >
+                                                    <option value="0">Chọn nguyên liệu</option>
+                                                    {nutritions && nutritions.map((nutrition) => (
+                                                        <option key={nutrition.id} value={nutrition.id}>
+                                                            {nutrition.name}
+                                                        </option>
+                                                    ))}
+                                                </select>
+                                            </Grid>
+                                            {fields.length === 1 ? '' : (
+                                                <Grid item md={1} xs={1} xl={1} lg={1} alignSelf="center">
+                                                    <button
+                                                        type="button"
+                                                        onClick={() => handleRemove(index)}
+                                                        className="text-red-500 hover:text-red-700"
+                                                    >
+                                                        <XMarkIcon width={24} height={24} />
+                                                    </button>
+                                                </Grid>
+                                            )}
+                                        </Grid>
+                                    ))}
+                                </Grid>
+                            </div>
+                        </div>
+                        <button
+                            type="button"
+                            onClick={handleAddMore}
+                            className="mt-4 bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 disabled:bg-gray-500"
+                            disabled={fields.length >= 3}
+                        >
+                            Thêm dinh dưỡng
+                        </button>
+                        <div className="flex justify-end">
+                            <button
+                                type="submit"
+                                className="bg-orange-500 text-white px-4 py-2 rounded-md hover:bg-orange-600"
+                                disabled={isLoading}
+                            >
+                                {isLoading ? (
+                                    <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
+                                        <circle
+                                            className="opacity-25"
+                                            cx="12"
+                                            cy="12"
+                                            r="10"
+                                            stroke="currentColor"
+                                            strokeWidth="4"
+                                        ></circle>
+                                        <path
+                                            className="opacity-75"
+                                            fill="currentColor"
+                                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                                        ></path>
+                                    </svg>
+                                ) : (
+                                    "Xác nhận"
+                                )}
+                            </button>
+                        </div>
+                    </form>
                 </DialogContent>
             </Dialog>
         )
