0f20af613786840357924a7150d15145e91421bd
NhutMinhIT <nhutminh.it19@gmail.com>
2024-07-23 00:44:38 +0700

    feat: revenue report of store model

---
+++ src/models/Statistic.ts
@@ -0,0 +1,25 @@
+// Purpose: Define the interface of Statistic model.
+export interface IBestSellingOfStore {
+    startDate: Date;
+    endDate: Date;
+    bestSellingProductTemplates: IBestSellingProductTemplates[]
+}
+export interface IBestSellingProductTemplates {
+    id: number
+    name: string;
+    urlImage: string
+    price: number;
+    sellingQuantity: number;
+    totalOrders: number;
+    totalRevenue: number
+}
+
+// Define the interface of IRevenueOfStore model.
+export interface IRevenueOfStore {
+    startDate: Date;
+    endDate: Date;
+    totalRevenue: number;
+    specificRevenue: number;
+    totalOrderCount: number;
+    specificOrderCount: number
+}
