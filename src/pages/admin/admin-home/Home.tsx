d5cda82db6fb87ae949ad031e3ce3e2f4e9e1b86
NhutMinhIT <nhutminh.it19@gmail.com>
2024-08-05 23:09:53 +0700

    refactor: route chart

--- src/pages/admin/admin-home/Home.tsx
+++ src/pages/admin/admin-home/Home.tsx
@@ -12,16 +12,16 @@ const Home = () => {
                     <SidebarComponent />
                 </div>
                 <div className="md:col-span-10 mt-20">
-                <div className="flex">
-                    <div className="flex flex-row text-3xl">
-                        <FaListAlt className="text-orange-500" />
-                        <h3 className="font-bold ml-6 uppercase">Thống kê</h3>
+                    <div className="flex">
+                        <div className="flex flex-row text-3xl">
+                            <FaListAlt className="text-orange-500" />
+                            <h3 className="font-bold ml-6 uppercase">Thống kê</h3>
+                        </div>
+                    </div>
+                    <div className="w-full overflow-auto">
+                        <RevenueReportAdmin />
                     </div>
                 </div>
-                <div className="w-full overflow-auto">
-                    <RevenueReportAdmin />
-                </div>
-            </div>
             </div>
         </div>
     )
